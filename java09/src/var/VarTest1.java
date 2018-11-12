68c340de960c5fa9eb7d573c3230b2f2bd1f2af0
jungwon lim <acliawill@naver.com>
2018-11-12 17:29:41 +0900

    upload55...

--- java09/src/var/VarTest1.java
+++ java09/src/var/VarTest1.java
@@ -1,46 +1,46 @@
 package var;
 
 public class VarTest1 {
-	int wide;//전체 영역에서 쓸 수 있다.
-			   //(전역변수)
+	int wide;//�쟾泥� �쁺�뿭�뿉�꽌 �벝 �닔 �엳�떎.
+			   //(�쟾�뿭蹂��닔)
 	
 	public void test() {
-		int temp;//선언!(지역변수)
+		int temp;//�꽑�뼵!(吏��뿭蹂��닔)
 		
 	}
 	public static void main(String[] args) {
-		//자바의 변수는 반드시 변수명 앞에
-		//변수에 저장할 데이타의 종류와 크기에 해당하는
-		//키워드를 써주어야 함.
-		byte age = 100; //= 대입, 할당
+		//�옄諛붿쓽 蹂��닔�뒗 諛섎뱶�떆 蹂��닔紐� �븵�뿉
+		//蹂��닔�뿉 ���옣�븷 �뜲�씠���쓽 醫낅쪟�� �겕湲곗뿉 �빐�떦�븯�뒗
+		//�궎�썙�뱶瑜� �뜥二쇱뼱�빞 �븿.
+		byte age = 100; //= ���엯, �븷�떦
 		int money = 300000;
 
-		//+ => 연결연산자
-		System.out.println("나의 나이는 " + age);
-		System.out.println("나의 은행 잔고는 " + money);
+		//+ => �뿰寃곗뿰�궛�옄
+		System.out.println("�굹�쓽 �굹�씠�뒗 " + age);
+		System.out.println("�굹�쓽 ���뻾 �옍怨좊뒗 " + money);
 		
 		float height = 185.5f;
 		double pi = 3.14584531; 
 		
-		char genger = '남'; //한글자=>'
+		//char genger = '�궓'; //�븳湲��옄=>'
 		
-		boolean hot = false;//선언+초기화(대입)
-		System.out.println("지금 더운가요? " + hot);
+		boolean hot = false;//�꽑�뼵+珥덇린�솕(���엯)
+		System.out.println("吏�湲� �뜑�슫媛��슂? " + hot);
 		
-		double weight;//쓰레기 값=> 선언.
-		//선언의 위치=> 사용할 수 있는 범위!!!!
+		double weight;//�벐�젅湲� 媛�=> �꽑�뼵.
+		//�꽑�뼵�쓽 �쐞移�=> �궗�슜�븷 �닔 �엳�뒗 踰붿쐞!!!!
 		
-		weight = 88.88;//=> 대입
-		System.out.println("나의 몸무게는 " + weight);
+		weight = 88.88;//=> ���엯
+		System.out.println("�굹�쓽 紐몃Т寃뚮뒗 " + weight);
 		
-		System.out.println("나의 성별은 " + genger);
-		
-		
+		//System.out.println("�굹�쓽 �꽦蹂꾩� " + genger);
 		
 		
 		
-		//""이중 따옴표
-		String welcome = "안녕하세요. 반갑습니다.";
+		
+		
+		//""�씠以� �뵲�샂�몴
+		String welcome = "�븞�뀞�븯�꽭�슂. 諛섍컩�뒿�땲�떎.";
 		
 		
 		

11cf9aaba43bf06dc670775eeacbe4d4bf81cc82
jungwon lim <acliawill@naver.com>
2018-11-13 13:26:22 +0900

    생성자로 기본 코드를

---
+++ java21/src/java21/SwingTest.java
@@ -0,0 +1,15 @@
+package java21;
+
+public class SwingTest {
+
+	
+	public SwingTest() {
+		//여기에 모든 소스를 넣어주세요....!!!
+	}
+	
+	
+	public static void main(String[] args) {
+		SwingTest swing = new SwingTest();
+	}
+
+}

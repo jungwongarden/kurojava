e45048eae3db800d28477eee298d13de7d57a220
jungwon lim <acliawill@naver.com>
2018-11-13 13:54:38 +0900

    up5

---
+++ java21/src/java21/SwingTest4.java
@@ -0,0 +1,13 @@
+package java21;
+
+public class SwingTest4 {
+
+	public SwingTest4() {
+		//내가 바로 처리될 코드들...
+		//나야 나...
+	}
+	public static void main(String[] args) {
+		SwingTest4 swing = new SwingTest4();
+	}
+
+}

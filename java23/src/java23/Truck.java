cb5a3d81be6487f5c6798767e60ea8c08b77314e
jungwon lim <acliawill@naver.com>
2018-11-15 10:00:48 +0900

    상속5

--- java23/src/java23/Truck.java
+++ java23/src/java23/Truck.java
@@ -4,9 +4,12 @@ public class Truck extends Car{
 	int load;
 	
 	public void move() {
+		System.out.println("=======");
+		
 		stop();
 		System.out.println("물건을 나르다.");
 		start();
+		System.out.println("=======");
 	}
 	
 	@Override

ff08f1a558b54dd9e87350d56504e7ad05eb9970
jungwon lim <acliawill@naver.com>
2019-01-02 10:10:14 +0900

    js

--- java54/WebContent/js/js01.js
+++ java54/WebContent/js/js01.js
@@ -3,6 +3,15 @@ function call1() {
 	var x2 = parseInt(x);
 	var result1 = addNum(x2);
 	alert(result1);
+	
+	var a = document.getElementById("a");
+	a.innerHTML = result1;
+	/*var test = a.innerHTML;*/
+	
+	var num1 = document.getElementById("num1");
+	num1.value = result1;
+//	var test2 = num1.value;
+	
 }
 function call2() {
 	var x = prompt("숫자1를 입력하세요.");
@@ -12,6 +21,10 @@ function call2() {
 	
 	var result2 = addNum2(x2, y2);
 	alert(result2);
+	var b = document.getElementById("b");
+	b.innerHTML = result2;
+	var num2 = document.getElementById("num2");
+	num2.value = result2;
 }
 function addNum(x) {
 	return x + 100;
